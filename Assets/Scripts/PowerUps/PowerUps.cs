96f7284920e20180d0e848b366b2764f06954310
Ella <er276953@falmouth.ac.uk>
2024-01-27 17:45:21 +0000

    Pickup whippy cream changed asset

--- Assets/Scripts/PowerUps/PowerUps.cs
+++ Assets/Scripts/PowerUps/PowerUps.cs
@@ -44,8 +44,15 @@ namespace PowerUps
 
         protected void CoolDown()
         {
-            // object mesh isn't visible till timer has finished
+            // object mesh isn't visible till timer has finished - will be fully removed when in game object have been updated
             this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+            // quick fix to replace mesh component hidden
+            if(this.gameObject.transform.GetChild(0))
+            {
+                this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            }
+
             coroutine = HideTimer(cooldownAmount);
             StartCoroutine(coroutine);
         }
@@ -54,6 +61,13 @@ namespace PowerUps
         {
             yield return new WaitForSeconds(time);
             this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+
+            if (this.gameObject.transform.GetChild(0))
+            {
+                this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            }
+
+
             collisionOccured = false;
         }
     }
