ae23086ffe42a6bef56fd2a8f49914925e0d9d09
Marshall Sharp <marshalljacob.sharp@gmail.com>
2024-01-27 15:35:13 +0000

    Ai should be able to move towards player

--- Assets/Scripts/NPC/AI_Behaviour.cs
+++ Assets/Scripts/NPC/AI_Behaviour.cs
@@ -5,13 +5,18 @@ using UnityEngine.AI;
 using UnityEngine;
 using UnityEditorInternal;
 using UnityEditor;
+using static UnityEditor.PlayerSettings;
 
 public class AI_Behaviour : MonoBehaviour
 {
     public Ai_stats stats;
     public Rigidbody body;
+    public GameObject Projectile, actorToHarm;
+    public Collider Meleehurtbox;
     public Vector3 Location;
     public float XEnd, ZEnd;
+
+    [SerializeField]
     float steps;
 
 
@@ -24,7 +29,7 @@ public class AI_Behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        randomMove();
+        MoveTowardsPlayer();
     }
 
     public void calculate_speed()
@@ -38,15 +43,29 @@ public class AI_Behaviour : MonoBehaviour
             steps = stats.speed * Time.deltaTime;
         }
     }
-        void randomMove()
+    void randomMove()
     {
         Location = transform.position;
         Vector3 pos = transform.position;
         pos = new Vector3(pos.x, pos.y, pos.z);
-        pos.x = Random.Range(1,XEnd);
+        pos.x = Random.Range(1, XEnd);
         //pos.y = Random.Range(1,YEnd);
-        pos.z = Random.Range(1,ZEnd);
-        transform.position = Vector3.MoveTowards(Location,pos, steps);
-        
+        pos.z = Random.Range(1, ZEnd);
+        calculate_speed();
+        transform.position = Vector3.MoveTowards(Location, pos, steps);
+
+    }
+
+    void MoveTowardsPlayer()
+    {
+        Location = transform.position;
+        Vector3 playerPos = actorToHarm.transform.position;
+        calculate_speed();
+        transform.position = Vector3.MoveTowards(Location, playerPos, steps);
+    }
+
+    private void OnCollisionEnter()
+    {
+      
     }
 }
