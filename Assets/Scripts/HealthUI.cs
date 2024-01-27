fbe7895a50ed9bfffb22506be3df53d9e4188932
MAYA DO <MD277193@falmouth.ac.uk>
2024-01-27 17:24:42 +0000

    added temp health bar ui

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public Image healthImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //healthImage.fillAmount = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //healthImage.fillAmount = (float) PlayerController.instance.currentHealth / PlayerController.instance.maxHealth;
+        //healthImage.fillAmount = Mathf.Clamp(healthImage.fillAmount, 0f, 1f);
+    }
+}
