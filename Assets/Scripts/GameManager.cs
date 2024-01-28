57eb4591b9a1b7b77541e600777e132494103908
MAYA DO <MD277193@falmouth.ac.uk>
2024-01-28 14:56:00 +0000

    added game manager + spawning

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int score = 0;
+
+    public float spawnDelay = 1f;
+
+    public Transform[] spawnPoints;
+    public Transform bossSpawnPoint;
+    private bool canSpawn = true;
+
+    public GameObject[] enemies;
+    public GameObject boss;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(EnemySpawner());
+    }
+    public void ChangeScore(int amount)
+    {
+        score += amount;
+    }
+
+    public IEnumerator EnemySpawner()
+    {
+        while (canSpawn)
+        {
+            yield return new WaitForSeconds(spawnDelay);
+            // choose random spawn position
+            int randomPosition = Random.Range(0, spawnPoints.Length);
+
+            // choose random enemy to spawn
+            int randomEnemy = Random.Range(0, enemies.Length);
+            GameObject enemyToSpawn = enemies[randomEnemy];
+
+            Instantiate(enemyToSpawn, spawnPoints[randomPosition].position, spawnPoints[randomPosition].rotation);
+        }
+    }
+}
