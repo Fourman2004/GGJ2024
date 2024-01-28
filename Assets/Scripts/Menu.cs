15e11aac952bff14aedb8055d9bcd6158aed1059
Marshall Sharp <marshalljacob.sharp@gmail.com>
2024-01-28 15:06:21 +0000

    Menu sorted

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEditor.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button StartButton, QuitButton, BackButton, Controls;
+    public Text ControlInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+   public void quitGame() 
+    { 
+        Application.Quit(); 
+    }
+
+    public void ShowControlScreen()
+    {
+        ControlInfo.gameObject.SetActive(true);
+        StartButton.gameObject.SetActive(false);
+        QuitButton.gameObject.SetActive(false);
+        BackButton.gameObject.SetActive(true);
+        Controls.gameObject.SetActive(false);
+        Debug.Log("Clicked");
+    }
+    public void back()
+    {
+        ControlInfo.gameObject.SetActive(false);
+        BackButton.gameObject.SetActive(false);
+        StartButton.gameObject.SetActive(true);
+        Controls.gameObject.SetActive(true);
+        QuitButton.gameObject.SetActive(true);
+
+    }
+
+}
